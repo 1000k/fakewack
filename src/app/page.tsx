e9025e584894807f7db742188cdf443c02f0e381
SENDA Keijiro <senda.keijiro@gmail.com>
2025-05-24 07:50:57 +0900

    feat: add Google Gen AI SDK package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,43 @@
-export default function Home() {
+
+import {
+  GoogleGenAI,
+} from '@google/genai';
+
+
+export default async function Home() {
+
+  const ai = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+  });
+  const config = {
+    responseMimeType: 'text/plain',
+  };
+  const model = 'gemini-2.5-flash-preview-05-20';
+  const contents = [
+    {
+      role: 'user',
+      parts: [
+        {
+          text: `次の文章を、SNS映えするキラキラ自慢文章に変換してください。「今日は1日中家でプログラミングをしていました。」`,
+        },
+      ],
+    },
+  ];
+
+  // const response = await ai.models.generateContentStream({
+  //   model,
+  //   config,
+  //   contents,
+  // });
+  // let text = '';
+  // for await (const chunk of response) {
+  //   console.log(chunk.text);
+  //   text += chunk.text;
+  // }
+
   return (
     <main>
-      <div>Hello world!</div>
+      <div>Home</div>
     </main>
   );
 }
