cc7dbee5ca15fee14b674bd1166db61b7e0e9c07
SENDA Keijiro <senda.keijiro@gmail.com>
2025-05-24 10:21:55 +0900

    fix header & footer design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Header />
-        {children}
+        <main className="max-w-2xl mx-auto flex justify-between px-4 py-4">{children}</main>
         <Footer/>
       </body> 
     </html>
