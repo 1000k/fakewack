953db129fb4dd05cd86422ffbbce59b10dc69477
SENDA Keijiro <senda.keijiro@gmail.com>
2025-05-26 08:55:20 +0900

    Fix loading spinner style

--- src/app/(content)/create/CreatePostForm.tsx
+++ src/app/(content)/create/CreatePostForm.tsx
@@ -1,6 +1,6 @@
-'use client';
-import { useActionState } from 'react';
-import { LoadingSpinner } from '@/components/spinner';
+"use client";
+import { useActionState } from "react";
+import { LoadingSpinner } from "@/components/spinner";
 
 type Props = {
   action: (formData: FormData) => Promise<void>;
@@ -24,14 +24,24 @@ export default function CreatePostForm({ action }: Props) {
         required
         disabled={pending}
       ></textarea>
-      <button
-        type="submit"
-        className="bg-blue-500 text-white px-4 py-2 rounded-md disabled:bg-gray-400 disabled:cursor-not-allowed"
-        disabled={pending}
-      >
-        {pending && <LoadingSpinner />}
-        {pending ? 'Boasting...' : '投稿'}
-      </button>
+
+      {pending ? (
+        <button
+          disabled
+          type="submit"
+          className="py-2.5 px-5 me-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 inline-flex items-center"
+        >
+          <LoadingSpinner />
+          Boasting...
+        </button>
+      ) : (
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center"
+        >
+          投稿
+        </button>
+      )}
     </form>
   );
 }
