c32686b7cb82687950c5d8779835d332ed434ce0
SENDA Keijiro <senda.keijiro@gmail.com>
2025-05-26 09:20:43 +0900

    fix(middleware): fix infinite redirect loop

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,15 +5,15 @@ export default auth((req) => {
   const isLoggedIn = !!req.auth;
   const isAuthPage = req.nextUrl.pathname.startsWith('/signin');
 
-  if (isAuthPage) {
-    if (isLoggedIn) {
-      return NextResponse.redirect(new URL('/', req.nextUrl.origin));
-    }
+  if (isAuthPage && isLoggedIn) {
+    return NextResponse.redirect(new URL('/', req.nextUrl.origin));
   }
 
-  if (!isLoggedIn) {
+  if (!isAuthPage && !isLoggedIn) {
     return NextResponse.redirect(new URL('/signin', req.nextUrl.origin));
   }
+
+  return NextResponse.next();
 });
 
 // Optionally, don't invoke Middleware on some paths
